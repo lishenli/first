7a9c7b1c21922fc6842ab717d7397cd4699d94ba
senli321 <senli321@163.com>
2017-11-30 23:02:23 +0800

    Initial commit

---
+++ src/main/java/testGit/Application.java
@@ -0,0 +1,22 @@
+package testGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Application.class, args);
+        System.out.println( "Hello World!" );
+    }
+}
